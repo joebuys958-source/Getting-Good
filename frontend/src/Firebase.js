9d8affd2e9d4824bdd00160259adbc7e1114c3fc
Joe <joebuys958@gmail.com>
2025-12-12 10:20:22 +0000

    Fix Vercel routing and latest inventory updates

--- frontend/src/Firebase.js
+++ frontend/src/Firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoRUhuwEoJWVtMYt1SQWAjlY2jho8sXkY",
-  authDomain: "resell-saas.firebaseapp.com",
-  projectId: "resell-saas",
-  storageBucket: "resell-saas.firebasestorage.app",
-  messagingSenderId: "551775226898",
-  appId: "1:551775226898:web:a13d7100a95278db1f9ccb"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
